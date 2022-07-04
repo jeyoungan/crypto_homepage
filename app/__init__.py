3b162e48402e7a39915a73b440a3a6b5c03669da
Jyan <jyan@autocrypt.io>
2022-07-04 14:53:25 +0900

    change 2 4

--- app/__init__.py
+++ app/__init__.py
@@ -70,7 +70,7 @@ def create_app(config_class=Config):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
+            file_handler = RotatingFileHandler('logs/crypto_homepage.log',
                                                maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter(
                 '%(asctime)s %(levelname)s: %(message)s '
